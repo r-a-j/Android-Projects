04bec16ca54f70dba8412b6a254c85d7543f91a9
Raj Pawar <er.rajpawar@gmail.com>
2018-07-12 14:55:00 +0530

    Guess Number game
    Guess number between 1 - 20 game

---
+++ GuessNumber/app/src/main/java/com/example/er_ra/guessnumber/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.er_ra.guessnumber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    int randomNumber;
+
+    public void generateRandomNumber () {
+
+        Random rand = new Random();
+        randomNumber = rand.nextInt(20) + 1;
+
+    }
+    public void guessFunction(View view) {
+
+        EditText numberEditText = (EditText) findViewById(R.id.numberEditText);
+        int guessedValue = Integer.parseInt(numberEditText.getText().toString());
+
+        if(guessedValue > 20 || guessedValue < 1) {
+            Toast.makeText(this, "Invalid Range", Toast.LENGTH_LONG).show();
+        }
+        else {
+            if(guessedValue > randomNumber) {
+                Toast.makeText(this, "Lower", Toast.LENGTH_SHORT).show();
+            }
+            if(guessedValue < randomNumber) {
+                Toast.makeText(this, "Higher", Toast.LENGTH_SHORT).show();
+            }
+            if(guessedValue == randomNumber) {
+                Toast.makeText(this, "Correct Congratzz..!", Toast.LENGTH_LONG).show();
+                generateRandomNumber();
+            }
+        }
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        generateRandomNumber();
+        
+    }
+}
